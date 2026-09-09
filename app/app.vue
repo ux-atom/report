4d96e05cf00ad36b4f343d71cbd45938af51b2e2
mohin7 <csemohin@gmail.com>
2026-09-09 19:14:30 +0600

    Add nuxt tailwind project
    Signed-off-by: mohin7 <csemohin@gmail.com>

---
+++ app/app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div class="bg-white p-8 rounded-lg shadow-lg">
+      <h1 class="text-3xl font-bold text-blue-600 mb-4">Nuxt & Tailwind CSS</h1>
+      <p class="text-gray-600">Your project is successfully configured!</p>
+    </div>
+  </div>
+</template>
